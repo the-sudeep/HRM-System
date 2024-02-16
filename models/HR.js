4b0de8c7f25964bd9e26074e8aaecd4a22eaf302
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-02-16 13:34:42 +0545

    modifications on HR model

--- models/HR.js
+++ models/HR.js
@@ -1,14 +1,6 @@
 const mongoose = require('mongoose');
 
 const hrSchema = new mongoose.Schema({
-    full_name : {
-        type : String, 
-        required : true,
-    },
-    username : {
-        type : String, 
-        required : true,
-    },
     email : {
         type : String, 
         required : true,
