019ac5e95bf227d17110fa3f84843aa4fc2eee36
Prabesh Dangi <dangiprabesh58@gmail.com>
2024-02-19 13:15:58 +0545

    Login,registration, auth middleware added.

---
+++ Routes/userRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controller/HRController");
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
+
+module.exports = router;
