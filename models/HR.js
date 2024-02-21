396f0ad1b12f1302aec7745b200bf3b50a2bdef7
Avay12 <avaygc@gmail.com>
2024-02-21 20:15:33 +0545

    changes in HR

--- models/HR.js
+++ models/HR.js
@@ -1,22 +1,33 @@
 const mongoose = require("mongoose");
 
-const hrSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 3,
-    maxlength: 20,
+const hrSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^\S+@\S+\.\S+$/,
-    lowercase: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const HR = mongoose.model("HR", hrSchema);
-
 module.exports = HR;
