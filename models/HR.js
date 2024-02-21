0507a9103a90b21df431a508fa9ef768f2b16b57
Avay12 <avaygc@gmail.com>
2024-02-21 19:51:40 +0545

    Validation addition

--- models/HR.js
+++ models/HR.js
@@ -1,17 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const hrSchema = new mongoose.Schema({
-    email : {
-        type : String, 
-        required : true,
-        unique : true,
-    },
-    password : {
-        type : String, 
-        required : true,
-    },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/,
+    lowercase: true,
+  },
 });
 
-const HR = mongoose.model('HR', hrSchema);
+const HR = mongoose.model("HR", hrSchema);
 
 module.exports = HR;
