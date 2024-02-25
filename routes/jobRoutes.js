28d52705e2fbf303c1e7e5800fd78ca9434f0ed4
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-02-25 16:51:55 +0545

    managed api endpoits

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -5,9 +5,9 @@ const {jobCreate, getAllJobs,getJobById, updateJob, deleteJob } = require('../co
 const tokenValidator = require('../middlewares/authMiddleware');
 
 router.post('/create',tokenValidator,jobCreate); // create a new job 
-router.get('/', getAllJobs);// get all the jobs 
-router.get('/:id', getJobById); //get job by ID 
-router.put('/:id', updateJob) // update the job by id 
-router.delete('/:id', deleteJob); // delete the job by ID
+router.get('/',tokenValidator, getAllJobs);// get all the jobs 
+router.get('/:id', tokenValidator,getJobById); //get job by ID 
+router.put('/update/:id',tokenValidator, updateJob) // update the job by id 
+router.delete('/delete/:id',tokenValidator, deleteJob); // delete the job by ID
 
 module.exports = router;
--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -3,7 +3,7 @@ const jobRoutes = require("../routes/jobRoutes");
 const applicantRoutes = require("../routes/applicantRoutes");
 const hrRoutes = require('../routes/userRoute');
 module.exports = function(app){
-    app.use("/jobs", jobRoutes);
-    app.use("/applicant",applicantRoutes)
-    app.use("/", hrRoutes);   
+    app.use("/api/jobs", jobRoutes);
+    app.use("/api/applicant",applicantRoutes)
+    app.use("/api/hr", hrRoutes);   
 }
